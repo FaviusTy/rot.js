b559dea62379711f4883540ef9eabff2e6b1d000
Favi_ty <favius983@gmail.com>
2017-12-28 21:18:49 +0900

    completed part1.

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,5 +1,6 @@
 import Display from '../../newSrc/display/Display'
 import Digger from '../../newSrc/map/Digger'
+import RNG from '../../newSrc/rng'
 
 class Game {
   constructor() {
@@ -7,6 +8,7 @@ class Game {
     this.map = {}
     document.body.appendChild(this.display.getContainer())
     this._generateMap()
+    this._generateBoxes([])
     this._drawWholeMap()
   }
 
@@ -20,9 +22,17 @@ class Game {
 
   _drawWholeMap() {
     Object.keys(this.map).forEach(vector => {
-      const [x, y] = vector
+      const [x, y] = vector.split(',')
       this.display.draw(parseInt(x), parseInt(y), this.map[vector])
     })
+  }
+
+  _generateBoxes(freeCells) {
+    for (let i = 0; i < 10; i++) {
+      const index = Math.floor(RNG.getUniform() * freeCells.length)
+      const key = freeCells.splice(index, 1)[0]
+      this.map[key] = '*'
+    }
   }
 }
 
